9690d67741b3d4fab733497d35762933a58f11eb
MarcoAlayn <marc_rob92@hotmail.com>
2022-10-21 06:42:04 -0700

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hola Mundo</h1>
+        <p>Hola Mundo</p>
       </header>
     </div>
   );
