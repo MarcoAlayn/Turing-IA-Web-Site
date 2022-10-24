7af41fffc9f34f8e7b3513c0e0efca78151cba65
MarcoAlayn <marc_rob92@hotmail.com>
2022-10-24 01:39:18 -0700

    componentResponsive weAreDoing

--- client/src/Pages/Inicio/index.js
+++ client/src/Pages/Inicio/index.js
@@ -2,11 +2,18 @@ import React from "react";
 import Header from "../../components/Header";
 import video from "../../assets/Inicio.mp4";
 import "./Inicio.css";
+import ButtonInicio from "../../components/ButtonInicio";
 
 function Inicio() {
   return (
     <div className="inicioContainer">
       <Header video={video} />
+      <h2 className="queHacemos">QUE HACEMOS</h2>
+      <p className="parrafo">
+        Turing-IA es una empresa de consultoría global de tecnología y negocios
+        que brinda soluciones que aportan al éxito de las empresas.
+      </p>
+      <ButtonInicio />
     </div>
   );
 }
---
+++ client/src/components/ButtonInicio/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ButtonInicio.css";
+
+function ButtonInicio() {
+  return (
+    <div className="buttonInicioContainer">
+      <button className="buttonInicioText">Conoce como</button>
+    </div>
+  );
+}
+
+export default ButtonInicio;
