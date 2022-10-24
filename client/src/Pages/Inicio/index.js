3fa59f2d993a389f554270978491b0dc3904b2c3
MarcoAlayn <marc_rob92@hotmail.com>
2022-10-23 18:06:14 -0700

    header responsive

--- client/src/Pages/Inicio/index.js
+++ client/src/Pages/Inicio/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Header from "../../components/Header";
 import video from "../../assets/Inicio.mp4";
+import "./Inicio.css";
 
 function Inicio() {
   return (
-    <div>
+    <div className="inicioContainer">
       <Header video={video} />
-      inicio
     </div>
   );
 }
